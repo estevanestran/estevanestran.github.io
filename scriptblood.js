16d30327e2c7ee34a66208b00e8583708c14d694
Estevan <111086438+estevanestran@users.noreply.github.com>
2024-12-22 15:16:01 -0300

    Update scriptblood.js

--- scriptblood.js
+++ scriptblood.js
@@ -1,4 +1,4 @@
-const blood50 = document.getElementById("blood729");
+const blood729 = document.getElementById("blood729");
 blood729.addEventListener("click", function () {
     alert("Você encontrou o ídolo da imunidade! Envie um print desse alerta no seu confessionário.");
 });
