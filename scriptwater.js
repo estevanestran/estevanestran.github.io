b1f85ffaa088f13cd38f69b7276be3febfe4472e
Estevan <111086438+estevanestran@users.noreply.github.com>
2024-12-22 15:18:13 -0300

    Add files via upload

---
+++ scriptwater.js
@@ -0,0 +1,4 @@
+const water438 = document.getElementById("water438");
+water438.addEventListener("click", function () {
+    alert("Você encontrou o ídolo da imunidade! Envie um print desse alerta no seu confessionário.");
+});
