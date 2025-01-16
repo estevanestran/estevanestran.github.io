9329f92ab0689c3176ab99bdda8dcd939d3a5162
Estevan <111086438+estevanestran@users.noreply.github.com>
2025-01-16 19:52:38 -0300

    Add files via upload

---
+++ scriptmerge.js
@@ -0,0 +1,200 @@
+const merge1 = document.getElementById("merge1");
+const merge2 = document.getElementById("merge2");
+const merge3 = document.getElementById("merge3");
+const merge4 = document.getElementById("merge4");
+const merge5 = document.getElementById("merge5");
+const merge6 = document.getElementById("merge6");
+const merge7 = document.getElementById("merge7");
+const merge8 = document.getElementById("merge8");
+const merge9 = document.getElementById("merge9");
+const merge10 = document.getElementById("merge10");
+const merge11 = document.getElementById("merge11");
+const merge12 = document.getElementById("merge12");
+const merge13 = document.getElementById("merge13");
+const merge14 = document.getElementById("merge14");
+const merge15 = document.getElementById("merge15");
+const merge16 = document.getElementById("merge16");
+const merge17 = document.getElementById("merge17");
+const merge18 = document.getElementById("merge18");
+const merge19 = document.getElementById("merge19");
+const merge20 = document.getElementById("merge20");
+const merge21 = document.getElementById("merge21");
+const merge22 = document.getElementById("merge22");
+const merge23 = document.getElementById("merge23");
+const merge24 = document.getElementById("merge24");
+const merge25 = document.getElementById("merge25");
+const merge26 = document.getElementById("merge26");
+const merge27 = document.getElementById("merge27");
+const merge28 = document.getElementById("merge28");
+const merge29 = document.getElementById("merge29");
+const merge30 = document.getElementById("merge30");
+const merge31 = document.getElementById("merge31");
+const merge32 = document.getElementById("merge32");
+const merge33 = document.getElementById("merge33");
+const merge34 = document.getElementById("merge34");
+const merge35 = document.getElementById("merge35");
+const merge36 = document.getElementById("merge36");
+const merge37 = document.getElementById("merge37");
+const merge38 = document.getElementById("merge38");
+const merge39 = document.getElementById("merge39");
+const merge40 = document.getElementById("merge40");
+const merge41 = document.getElementById("merge41");
+const merge42 = document.getElementById("merge42");
+const merge43 = document.getElementById("merge43");
+const merge44 = document.getElementById("merge44");
+const merge45 = document.getElementById("merge45");
+const merge46 = document.getElementById("merge46");
+const merge47 = document.getElementById("merge47");
+const merge48 = document.getElementById("merge48");
+const merge49 = document.getElementById("merge49");
+const merge50 = document.getElementById("merge50");
+const merge51 = document.getElementById("merge51");
+const merge52 = document.getElementById("merge52");
+const merge53 = document.getElementById("merge53");
+const merge54 = document.getElementById("merge54");
+const merge55 = document.getElementById("merge55");
+const merge56 = document.getElementById("merge56");
+const merge57 = document.getElementById("merge57");
+const merge58 = document.getElementById("merge58");
+const merge59 = document.getElementById("merge59");
+const merge60 = document.getElementById("merge60");
+const merge61 = document.getElementById("merge61");
+const merge62 = document.getElementById("merge62");
+const merge63 = document.getElementById("merge63");
+const merge64 = document.getElementById("merge64");
+const merge65 = document.getElementById("merge65");
+const merge66 = document.getElementById("merge66");
+const merge67 = document.getElementById("merge67");
+const merge68 = document.getElementById("merge68");
+const merge69 = document.getElementById("merge69");
+const merge70 = document.getElementById("merge70");
+const merge71 = document.getElementById("merge71");
+const merge72 = document.getElementById("merge72");
+const merge73 = document.getElementById("merge73");
+const merge74 = document.getElementById("merge74");
+const merge75 = document.getElementById("merge75");
+const merge76 = document.getElementById("merge76");
+const merge77 = document.getElementById("merge77");
+const merge78 = document.getElementById("merge78");
+const merge79 = document.getElementById("merge79");
+const merge80 = document.getElementById("merge80");
+const merge81 = document.getElementById("merge81");
+const merge82 = document.getElementById("merge82");
+const merge83 = document.getElementById("merge83");
+const merge84 = document.getElementById("merge84");
+const merge85 = document.getElementById("merge85");
+const merge86 = document.getElementById("merge86");
+const merge87 = document.getElementById("merge87");
+const merge88 = document.getElementById("merge88");
+const merge89 = document.getElementById("merge89");
+const merge90 = document.getElementById("merge90");
+const merge91 = document.getElementById("merge91");
+const merge92 = document.getElementById("merge92");
+const merge93 = document.getElementById("merge93");
+const merge94 = document.getElementById("merge94");
+const merge95 = document.getElementById("merge95");
+const merge96 = document.getElementById("merge96");
+const merge97 = document.getElementById("merge97");
+const merge98 = document.getElementById("merge98");
+const merge99 = document.getElementById("merge99");
+const merge100 = document.getElementById("merge100");
+merge1.addEventListener("click", function () { alert("Você NÃO encontrou o ídolo da imunidade. Continue procurando.");})
+merge2.addEventListener("click", function () { alert("Você NÃO encontrou o ídolo da imunidade. Continue procurando.");})
+merge3.addEventListener("click", function () { alert("Você NÃO encontrou o ídolo da imunidade. Continue procurando.");})
+merge4.addEventListener("click", function () { alert("Você NÃO encontrou o ídolo da imunidade. Continue procurando.");})
+merge5.addEventListener("click", function () { alert("Você NÃO encontrou o ídolo da imunidade. Continue procurando.");})
+merge6.addEventListener("click", function () { alert("Você NÃO encontrou o ídolo da imunidade. Continue procurando.");})
+merge7.addEventListener("click", function () { alert("Você NÃO encontrou o ídolo da imunidade. Continue procurando.");})
+merge8.addEventListener("click", function () { alert("Você NÃO encontrou o ídolo da imunidade. Continue procurando.");})
+merge9.addEventListener("click", function () { alert("Você NÃO encontrou o ídolo da imunidade. Continue procurando.");})
+merge10.addEventListener("click", function () { alert("Você NÃO encontrou o ídolo da imunidade. Continue procurando.");})
+merge11.addEventListener("click", function () { alert("Você NÃO encontrou o ídolo da imunidade. Continue procurando.");})
+merge12.addEventListener("click", function () { alert("Você NÃO encontrou o ídolo da imunidade. Continue procurando.");})
+merge13.addEventListener("click", function () { alert("Você NÃO encontrou o ídolo da imunidade. Continue procurando.");})
+merge14.addEventListener("click", function () { alert("Você NÃO encontrou o ídolo da imunidade. Continue procurando.");})
+merge15.addEventListener("click", function () { alert("Você NÃO encontrou o ídolo da imunidade. Continue procurando.");})
+merge16.addEventListener("click", function () { alert("Você NÃO encontrou o ídolo da imunidade. Continue procurando.");})
+merge17.addEventListener("click", function () { alert("Você NÃO encontrou o ídolo da imunidade. Continue procurando.");})
+merge18.addEventListener("click", function () { alert("Você NÃO encontrou o ídolo da imunidade. Continue procurando.");})
+merge19.addEventListener("click", function () { alert("Você NÃO encontrou o ídolo da imunidade. Continue procurando.");})
+merge20.addEventListener("click", function () { alert("Você NÃO encontrou o ídolo da imunidade. Continue procurando.");})
+merge21.addEventListener("click", function () { alert("Você NÃO encontrou o ídolo da imunidade. Continue procurando.");})
+merge22.addEventListener("click", function () { alert("Você NÃO encontrou o ídolo da imunidade. Continue procurando.");})
+merge23.addEventListener("click", function () { alert("Você NÃO encontrou o ídolo da imunidade. Continue procurando.");})
+merge24.addEventListener("click", function () { alert("Você NÃO encontrou o ídolo da imunidade. Continue procurando.");})
+merge25.addEventListener("click", function () { alert("Você NÃO encontrou o ídolo da imunidade. Continue procurando.");})
+merge26.addEventListener("click", function () { alert("Você NÃO encontrou o ídolo da imunidade. Continue procurando.");})
+merge27.addEventListener("click", function () { alert("Você NÃO encontrou o ídolo da imunidade. Continue procurando.");})
+merge28.addEventListener("click", function () { alert("Você NÃO encontrou o ídolo da imunidade. Continue procurando.");})
+merge29.addEventListener("click", function () { alert("Você NÃO encontrou o ídolo da imunidade. Continue procurando.");})
+merge30.addEventListener("click", function () { alert("Você NÃO encontrou o ídolo da imunidade. Continue procurando.");})
+merge31.addEventListener("click", function () { alert("Você NÃO encontrou o ídolo da imunidade. Continue procurando.");})
+merge32.addEventListener("click", function () { alert("Você NÃO encontrou o ídolo da imunidade. Continue procurando.");})
+merge33.addEventListener("click", function () { alert("Você NÃO encontrou o ídolo da imunidade. Continue procurando.");})
+merge34.addEventListener("click", function () { alert("Você NÃO encontrou o ídolo da imunidade. Continue procurando.");})
+merge35.addEventListener("click", function () { alert("Você NÃO encontrou o ídolo da imunidade. Continue procurando.");})
+merge36.addEventListener("click", function () { alert("Você NÃO encontrou o ídolo da imunidade. Continue procurando.");})
+merge37.addEventListener("click", function () { alert("Você NÃO encontrou o ídolo da imunidade. Continue procurando.");})
+merge38.addEventListener("click", function () { alert("Você NÃO encontrou o ídolo da imunidade. Continue procurando.");})
+merge39.addEventListener("click", function () { alert("Você NÃO encontrou o ídolo da imunidade. Continue procurando.");})
+merge40.addEventListener("click", function () { alert("Você NÃO encontrou o ídolo da imunidade. Continue procurando.");})
+merge41.addEventListener("click", function () { alert("Você NÃO encontrou o ídolo da imunidade. Continue procurando.");})
+merge42.addEventListener("click", function () { alert("Você NÃO encontrou o ídolo da imunidade. Continue procurando.");})
+merge43.addEventListener("click", function () { alert("Você NÃO encontrou o ídolo da imunidade. Continue procurando.");})
+merge44.addEventListener("click", function () { alert("Você NÃO encontrou o ídolo da imunidade. Continue procurando.");})
+merge45.addEventListener("click", function () { alert("Você NÃO encontrou o ídolo da imunidade. Continue procurando.");})
+merge46.addEventListener("click", function () { alert("Você NÃO encontrou o ídolo da imunidade. Continue procurando.");})
+merge47.addEventListener("click", function () { alert("Você NÃO encontrou o ídolo da imunidade. Continue procurando.");})
+merge48.addEventListener("click", function () { alert("Você NÃO encontrou o ídolo da imunidade. Continue procurando.");})
+merge49.addEventListener("click", function () { alert("Você NÃO encontrou o ídolo da imunidade. Continue procurando.");})
+merge50.addEventListener("click", function () { alert("Você NÃO encontrou o ídolo da imunidade. Continue procurando.");})
+merge51.addEventListener("click", function () { alert("Você NÃO encontrou o ídolo da imunidade. Continue procurando.");})
+merge52.addEventListener("click", function () { alert("Você NÃO encontrou o ídolo da imunidade. Continue procurando.");})
+merge53.addEventListener("click", function () { alert("Você NÃO encontrou o ídolo da imunidade. Continue procurando.");})
+merge54.addEventListener("click", function () { alert("Você NÃO encontrou o ídolo da imunidade. Continue procurando.");})
+merge55.addEventListener("click", function () { alert("Você NÃO encontrou o ídolo da imunidade. Continue procurando.");})
+merge56.addEventListener("click", function () { alert("Você NÃO encontrou o ídolo da imunidade. Continue procurando.");})
+merge57.addEventListener("click", function () { alert("Você NÃO encontrou o ídolo da imunidade. Continue procurando.");})
+merge58.addEventListener("click", function () { alert("Você NÃO encontrou o ídolo da imunidade. Continue procurando.");})
+merge59.addEventListener("click", function () { alert("Você NÃO encontrou o ídolo da imunidade. Continue procurando.");})
+merge60.addEventListener("click", function () { alert("Você NÃO encontrou o ídolo da imunidade. Continue procurando.");})
+merge61.addEventListener("click", function () { alert("Você NÃO encontrou o ídolo da imunidade. Continue procurando.");})
+merge62.addEventListener("click", function () { alert("Você NÃO encontrou o ídolo da imunidade. Continue procurando.");})
+merge63.addEventListener("click", function () { alert("Você NÃO encontrou o ídolo da imunidade. Continue procurando.");})
+merge64.addEventListener("click", function () { alert("Você NÃO encontrou o ídolo da imunidade. Continue procurando.");})
+merge65.addEventListener("click", function () { alert("Você NÃO encontrou o ídolo da imunidade. Continue procurando.");})
+merge66.addEventListener("click", function () { alert("Você NÃO encontrou o ídolo da imunidade. Continue procurando.");})
+merge67.addEventListener("click", function () { alert("Você NÃO encontrou o ídolo da imunidade. Continue procurando.");})
+merge68.addEventListener("click", function () { alert("Você NÃO encontrou o ídolo da imunidade. Continue procurando.");})
+merge69.addEventListener("click", function () { alert("Você NÃO encontrou o ídolo da imunidade. Continue procurando.");})
+merge70.addEventListener("click", function () { alert("Você NÃO encontrou o ídolo da imunidade. Continue procurando.");})
+merge71.addEventListener("click", function () { alert("Você NÃO encontrou o ídolo da imunidade. Continue procurando.");})
+merge72.addEventListener("click", function () { alert("Você NÃO encontrou o ídolo da imunidade. Continue procurando.");})
+merge73.addEventListener("click", function () { alert("Você NÃO encontrou o ídolo da imunidade. Continue procurando.");})
+merge74.addEventListener("click", function () { alert("Você NÃO encontrou o ídolo da imunidade. Continue procurando.");})
+merge75.addEventListener("click", function () { alert("Você NÃO encontrou o ídolo da imunidade. Continue procurando.");})
+merge76.addEventListener("click", function () { alert("Você NÃO encontrou o ídolo da imunidade. Continue procurando.");})
+merge77.addEventListener("click", function () { alert("Você NÃO encontrou o ídolo da imunidade. Continue procurando.");})
+merge78.addEventListener("click", function () { alert("Você NÃO encontrou o ídolo da imunidade. Continue procurando.");})
+merge79.addEventListener("click", function () { alert("Você NÃO encontrou o ídolo da imunidade. Continue procurando.");})
+merge80.addEventListener("click", function () { alert("Você NÃO encontrou o ídolo da imunidade. Continue procurando.");})
+merge81.addEventListener("click", function () { alert("Você NÃO encontrou o ídolo da imunidade. Continue procurando.");})
+merge82.addEventListener("click", function () { alert("Você NÃO encontrou o ídolo da imunidade. Continue procurando.");})
+merge83.addEventListener("click", function () { alert("Você NÃO encontrou o ídolo da imunidade. Continue procurando.");})
+merge84.addEventListener("click", function () { alert("Você NÃO encontrou o ídolo da imunidade. Continue procurando.");})
+merge85.addEventListener("click", function () { alert("Você NÃO encontrou o ídolo da imunidade. Continue procurando.");})
+merge86.addEventListener("click", function () { alert("Você NÃO encontrou o ídolo da imunidade. Continue procurando.");})
+merge87.addEventListener("click", function () { alert("Você NÃO encontrou o ídolo da imunidade. Continue procurando.");})
+merge88.addEventListener("click", function () { alert("Você NÃO encontrou o ídolo da imunidade. Continue procurando.");})
+merge89.addEventListener("click", function () { alert("Você NÃO encontrou o ídolo da imunidade. Continue procurando.");})
+merge90.addEventListener("click", function () { alert("Você NÃO encontrou o ídolo da imunidade. Continue procurando.");})
+merge91.addEventListener("click", function () { alert("Você NÃO encontrou o ídolo da imunidade. Continue procurando.");})
+merge92.addEventListener("click", function () { alert("Você NÃO encontrou o ídolo da imunidade. Continue procurando.");})
+merge93.addEventListener("click", function () { alert("Você NÃO encontrou o ídolo da imunidade. Continue procurando.");})
+merge94.addEventListener("click", function () { alert("Você NÃO encontrou o ídolo da imunidade. Continue procurando.");})
+merge95.addEventListener("click", function () { alert("Você NÃO encontrou o ídolo da imunidade. Continue procurando.");})
+merge96.addEventListener("click", function () { alert("Você NÃO encontrou o ídolo da imunidade. Continue procurando.");})
+merge97.addEventListener("click", function () { alert("Você NÃO encontrou o ídolo da imunidade. Continue procurando.");})
+merge98.addEventListener("click", function () { alert("Você NÃO encontrou o ídolo da imunidade. Continue procurando.");})
+merge99.addEventListener("click", function () { alert("Você NÃO encontrou o ídolo da imunidade. Continue procurando.");})
+merge100.addEventListener("click", function () { alert("Você NÃO encontrou o ídolo da imunidade. Continue procurando.");})
